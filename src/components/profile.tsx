59d687f5b6e4cef57d7942ee608c39dd11c57bb5
Yoshihiro Koyama <me@yox5ro.net>
2025-06-29 14:07:06 +0900

    udpate content

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -24,19 +24,32 @@ export default function Profile() {
 							Bachelor of Agricultural Science at Kobe University, 2019-2023
 						</Li>
 					</ul>
-					<H2 title="Publications" className="mt-8" />
+					<H2 title="Peer reviewed publications" className="mt-8" />
 					<ul>
-						<Li>
+						<Li className="mt-2">
 							<b>Koyama, Y.</b>, Nasu, M., & Matsuoka, Y. (2025). <i>f</i>
 							<sub>4</sub>-statistics-based ancestry profiling and convolutional
 							neural network phenotyping shed new light on the structure of
 							genetic and spike shape diversity in <i>Aegilops tauschii</i>{" "}
-							Coss. <i>bioRxiv</i> (preprint).{" "}
+							Coss. <i>Proceedings of the Japan Academy, Series B</i>.{" "}
 							<a
 								className="hover:opacity-60 underline"
-								href="https://doi.org/10.1101/2025.02.16.638466"
+								href="	https://doi.org/10.2183/pjab.101.023"
 							>
-								https://doi.org/10.1101/2025.02.16.638466
+								https://doi.org/10.2183/pjab.101.023
+							</a>
+						</Li>
+						<Li className="mt-2">
+							<b>Koyama, Y.</b>, Tanaka, H., Sato, K., Tsujimoto H., Matsuoka,
+							Y. (2025). Accessions of the genetically distinct TauL3 lineage of{" "}
+							<i>Aegilops tauschii</i> Coss. from newly identified habitats in
+							Armenia, Azerbaijan, and Iran.{" "}
+							<i>Genetic Resources and Crop Evolution</i>.{" "}
+							<a
+								className="hover:opacity-60 underline"
+								href="https://doi.org/10.1007/s10722-025-02438-4"
+							>
+								https://doi.org/10.1007/s10722-025-02438-4
 							</a>
 						</Li>
 					</ul>
